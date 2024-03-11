6c9f5a2589d43da27cdf004da6f39e9c1b0c4b44
Braden <bradenoptimus@gmail.com>
2024-03-10 18:50:35 -0700

    loops

---
+++ Golang/loops.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 0 
+	for i < 3 {
+		fmt.Println(i)
+		i++
+	}
+
+	for j := 0; j < 4; j++ {
+		if j % 2 == 0 {
+			fmt.Printf("%d is even\n", j)
+		} else if j % 3 == 0 {
+			fmt.Printf("%d is divisible by three\n", j)
+		}
+	}
+}
