d1cee7d602b58649d42bd75f10a4f43bd25f8c5b
Braden <bradenoptimus@gmail.com>
2024-03-11 16:11:43 -0700

    variables

---
+++ perl/hello.pl
@@ -0,0 +1,13 @@
+#!usr/bin/perl
+use warnings;
+use strict; #requires that all variables be initialized
+
+# $ starts of a scalar
+# my makes something not global
+my $word = "These are words";
+my $numb = '64';
+
+# 
+
+print($word, "\n");
+print "this is a $numb number\n";
