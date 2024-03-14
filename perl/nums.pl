b58c8e14f5ec2e0e69bdb09ca9098231012ebfdd
Braden <bradenoptimus@gmail.com>
2024-03-14 16:20:01 -0700

    simple hash

--- perl/nums.pl
+++ perl/nums.pl
@@ -4,6 +4,12 @@ use warnings;
 
 my $x = 123_456_789;
 my $inp;
+my @arr1 = qw(thing thang theng);
+my @arr2 = (1, 4, 16);
+
 chomp($inp = <STDIN>);
+
 print($x, "\n");
 print($inp, "\n");
+print($arr1[0], "\n");
+print($arr2[2], "\n");
--- perl/third.pl
+++ perl/third.pl
@@ -1,3 +1,13 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+
+my %thing = (
+    word => 1,
+    werd => 2,
+);
+
+my $res;
+chomp($res = <STDIN>);
+
+print($thing{$res}, "\n");
