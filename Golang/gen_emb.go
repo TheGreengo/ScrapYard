cff7b01575eeccde4c7836ddd59b145057d79357
Braden <bradenoptimus@gmail.com>
2024-04-02 22:28:42 -0700

    generally ok

--- Golang/gen_emb.go
+++ Golang/gen_emb.go
@@ -6,6 +6,16 @@ type magnitude struct {
 	size float64
 }
 
+func anyPrint[T any](thing T) {
+	fmt.Println(thing)
+}
+
+type node[T any] struct {
+	val T 
+	left * node[T] 
+	right * node[T]
+}
+
 type vector struct {
 	magnitude
 	direction float64
@@ -20,4 +30,14 @@ func main() {
 	}
 
 	fmt.Println(thing)
+	anyPrint(4)
+	anyPrint("hello")
+
+	thang := node[int]{val: 4, left: nil, right: nil} 
+	theng := node[int]{val: 6, left: nil, right: nil} 
+	thung := node[int]{val: 5, left: &thang, right: &theng} 
+	
+	fmt.Println("center:", thung.val)
+	fmt.Println("left:", (*thung.left).val)
+	fmt.Println("right:", (*thung.right).val)
 }
