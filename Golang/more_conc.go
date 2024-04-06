8c81312993d7464963a071806fa75e2692c0f4b3
Braden <bradenoptimus@gmail.com>
2024-04-05 20:57:04 -0700

    dumb mistakes all around

--- Golang/more_conc.go
+++ Golang/more_conc.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func ffffft(word chan<- string, ward string) {
@@ -13,9 +14,28 @@ func tfffff(word <-chan string) {
 	fmt.Println(ward)
 }
 
+func timeDel(word chan<- string, del int, mess string) {
+	time.Sleep(time.Duration(del) * time.Second)
+	word <- mess
+}
+
 func main() {
 	thing := make(chan string, 1)
+	thang := make(chan string, 1)
+	theng := make(chan string, 1)
 
 	ffffft(thing, "butt")
 	tfffff(thing)
+
+	go timeDel(thang, 1, "first")
+	go timeDel(theng, 2, "second")
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-thang:
+			fmt.Println(msg1)
+		case msg2 := <-theng:
+			fmt.Println(msg2)
+		}
+	}
 }
