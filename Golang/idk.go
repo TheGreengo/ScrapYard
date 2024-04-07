3f13f1de0e6d7da44e30a8dd87efd71d9ba7c965
Braden <bradenoptimus@gmail.com>
2024-04-07 02:01:23 -0700

    this seems marginally better than worthless

--- Golang/idk.go
+++ Golang/idk.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	thing := make(chan int, 4)
+	tid := time.NewTimer(time.Second)
+	tid2 := time.NewTimer(time.Second * time.Duration(3))
 	
 	for i := 4; i > 0; i -- {
 		thing <- i
@@ -16,4 +19,14 @@ func main() {
 	for j := range thing {
 		fmt.Println(j)
 	}
+
+	<-tid.C
+
+	go func() {
+		<-tid2.C
+	} ()
+	
+	thh := tid2.Stop()
+
+	fmt.Println(thh)
 }
