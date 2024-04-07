824e8eec3148097f4573173d5a9be78d3868e9da
Braden <bradenoptimus@gmail.com>
2024-04-06 23:23:51 -0700

    closing things up

--- Golang/non-block.go
+++ Golang/non-block.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -26,4 +27,29 @@ func main() {
 	go func() {thing <- true}()
 	val := <-thing
 	fmt.Println(val)
+
+	theng := make(chan int, 4)
+
+	go func () {
+		for {
+			i, more := <-theng
+			if !more {
+				fmt.Println("donzo")
+				return
+			}
+			fmt.Println(i)
+		}
+	} ()
+
+	for j := 0; j < 4; j++ {
+		theng <- j
+	}
+
+	time.Sleep(time.Second)
+
+	close(theng)
+	_, good := <-theng
+	if (good) {
+		fmt.Println("Closed successfully")
+	}
 }
