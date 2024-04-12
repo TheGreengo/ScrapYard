c255539d1cd980fcd58f41075fdd84941623595b
Braden <bradenoptimus@gmail.com>
2024-04-11 20:11:47 -0700

    that one's weird

--- Golang/pool.go
+++ Golang/pool.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func thing_doer(name string, thing chan int, out chan int) {
+	for i := range thing {
+		fmt.Println("Running job:",i ,"on:", name)
+		time.Sleep(time.Second)
+		fmt.Println("Done with job:",i ,"on:", name)
+		out <- i
+	}
+}
+
 func main() {
+	things := make(chan int, 8)
+	outs := make(chan int, 8)
+
+	names := []string{"a","b","c","d"}
+
+	for i := 0; i < 4; i++ {
+		go thing_doer(names[i], things, outs)
+	}
+	
+	for i := 0; i < 9; i++ {
+		things <- i
+	}
+	
+	for i := 0; i < 9; i++ {
+		num := <-outs
+		fmt.Println(num)
+	}
 	
 }
