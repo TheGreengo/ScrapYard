15379274a9686b29a1ff6414513e764651ea336d
Braden <bradenoptimus@gmail.com>
2024-04-14 21:25:36 -0700

    actually working with matrices now

--- Golang/mat/mat_mult.go
+++ Golang/mat/mat_mult.go
@@ -1,10 +1,9 @@
 package main
 
-func mult() float64 {
-	// thing := make([][]float64, 4)
-	// for i := range thing {
-	// 	thing[i] = []float64{1,0,0,0}
-	// }
-	// return thing
-	return 3.14159265
+func mult() [][]float64 {
+	thing := make([][]float64, 4)
+	for i := range thing {
+		thing[i] = []float64{1,0,0,0}
+	}
+	return thing
 }
