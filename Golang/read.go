5ac56d91715ace02ef52a3c718bb19cf17dbae8b
Braden <bradenoptimus@gmail.com>
2024-05-02 23:06:56 -0700

    bit by boring bit (byte)

--- Golang/read.go
+++ Golang/read.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"bufio"
+	"errors"
+	"io"
 )
 
 /*
@@ -10,14 +13,31 @@ import (
 * I want it to:
 * - take in command line arguments (file name, encrypt/decrypt, key)
 * - then I want it to read every byte, and change it to being the 
+* here we're just going to open a file and then read the bits
 */
 
 func main() {
-	dat, err := os.ReadFile("./test.txt")
+	file, err := os.Open("./test.txt")
 	
 	if err != nil {
 		panic(err)
 	}
+	
+	in_the_buff := bufio.NewReader(file)
+	
+	for {
+		b, er := in_the_buff.ReadByte()	
+	
+		if errors.Is(er, io.EOF) {
+			fmt.Printf("%d\n", b)
+			fmt.Println("End of line")
+			break
+		}
 
-	fmt.Println(len(dat))	
+		if er != nil {
+			panic(er)
+		}
+	
+		fmt.Printf("%d\n", b)
+	}
 }
