3ed1ca3caf3cbb525434c297c8cc967f62e06357
Braden <bradenoptimus@gmail.com>
2024-06-07 08:47:17 -0700

    we have now served a thhing

--- Golang/test_temp.go
+++ Golang/test_temp.go
@@ -2,8 +2,21 @@ package main
 
 import (
     "html/template"
+    "net/http"
 )
 
+type Page struct {
+    Title string
+    Text string
+}
+
 func main() {
+    pg := Page{Title:"The Name", Text:"was successful"}
+    tmp := template.Must(template.ParseFiles("test.html"))
     
+    http.HandleFunc("/test", func (w http.ResponseWriter, r *http.Request) {
+        tmp.Execute(w, pg)
+    })
+
+    http.ListenAndServe(":420", nil)
 }
